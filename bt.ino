feebc3c50f708cb74c3967d727f61935f7c29a5f
aditmittal <45120598+aditmittal@users.noreply.github.com>
2019-10-14 14:06:28 +0530

    14102019

---
+++ bt.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(2,3);
+#define button 8
+//int state = 20;
+//int buttonState = 0;
+int data;
+int led1=11;
+int led2=12;
+int led3=13;
+void setup() {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  //pinMode(button, INPUT);
+   BT.begin(38400);
+   Serial.begin(9600);
+   Serial.println("Slave Bluetooth");
+}
+
+void loop() {
+ if(Serial.available())
+ {
+  int data  = Serial.read();
+  delay(100);
+  if(data=="1")
+  {
+    digitalWrite(led3,HIGH);
+  }
+ }
+   
+}
---
+++ gsm.ino
@@ -0,0 +1,49 @@
+#include<SoftwareSerial.h>
+
+SoftwareSerial mySerial(9,10);
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0)
+  {
+    switch(Serial.read())
+    {
+      case 's';
+        sendmsg();
+        break;
+       case 'r':
+        recievemsg();
+        break;
+    }
+    if mySerial.available()>0)
+    {
+      Serial.write(mySerial.read());
+    }
+  }
+}
+void sendmsg()
+{
+  mySerial.println("AT+CMGF = 1");
+  delay(1000);
+  mySerial.println("AT+CMGS =\"+91 xxxxxxxxxx\"\r");
+  delay(1000);
+  mySerial.println("I am sms from gsm module");
+  delay(1000);
+  mySerial.println((char)26);
+  delay(1000);
+}
+
+void recievemsg()
+{
+  mySerial.println("AT+CNMI = 2,2,0,0,0");
+  delay(1000);
+}
+
